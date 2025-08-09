dd7f504c73ff09e75cd281e51e34567c0111d83d
Raging Smurf <shark.boom.nail@cloak.id>
2025-08-09 12:44:36 -0500

    Use SSH key ID r006-ce9f3f60-822a-415c-9787-598c0c5b9d52 for lab_vsi

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,5 +33,5 @@ resource "ibm_is_instance" "lab_vsi" {
   }
   vpc  = ibm_is_vpc.lab_vpc.id
   zone = "${var.ibm_region}-1"
-  keys = [var.ssh_key_name]
+  keys = ["r006-ce9f3f60-822a-415c-9787-598c0c5b9d52"]
 }
